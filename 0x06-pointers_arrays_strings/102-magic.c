173480f11e832e2bb027458b6663fe91a6320987
“payneprince” <tackiep7@gmail.com>
2023-06-30 09:24:47 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,18 +3,28 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-	int a[5] = {0};
+	int a[5];
 	int *p;
 
 	a[2] = 1024;
-	p = &a[2];
+	p = &n;
+
+	/*
+	 * Write your line of code here...
+	 * Remember:
+	 * - You are not allowed to use 'a'
+	 * - You are not allowed to modify 'p'
+	 * - Only one statement
+	 * - You are not allowed to code anything else than this line of code
+	 */
 	*(p + 5) = 98;
 
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
